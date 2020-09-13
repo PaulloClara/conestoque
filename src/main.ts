5c0fa7bc26984d911fc08100c0287f0cb6229368
paulloclara <paulloclara@gmail.com>
2020-09-13 16:03:00 -0300

    :fire: Move configuracao do Primevue
    > Codigo movido para arquivo proprio de plugins

--- src/main.ts
+++ src/main.ts
@@ -1,12 +1,11 @@
 import Vue from "vue";
+
 import store from "@/store";
 import router from "@/router";
 import App from "@/App.vue";
 
+import "@/plugins/primevue";
 import "@/registerServiceWorker";
-
-import "primevue/resources/primevue.min.css";
-import "primevue/resources/themes/saga-blue/theme.css";
 import "@/assets/styles/main.scss";
 
 Vue.config.productionTip = false;
---
+++ src/plugins/primevue.ts
@@ -0,0 +1,8 @@
+import Vue from "vue";
+
+import PButton from "primevue/button";
+
+import "primevue/resources/primevue.min.css";
+import "primevue/resources/themes/saga-blue/theme.css";
+
+Vue.component("p-button", PButton);
